78178ac8b2de28a4114f5a960f52893cea492a8f
Jack <pjh31208@naver.com>
2021-11-29 16:19:26 +0900

    NaviIng

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,20 @@
 
-$(" #click ").click(function () {
-	$(" #s-d ").toggleClass('show');
+$(" #DOG ").click(function () {
+	$(" #drop1 ").toggleClass('show');
+})
+
+$(" #CAT ").click(function () {
+	$(" #drop2 ").toggleClass('show');
+})
+
+$(" #LIVING ").click(function () {
+	$(" #drop3 ").toggleClass('show');
+})
+
+$(" #FOOD ").click(function () {
+	$(" #drop4 ").toggleClass('show');
+})
+
+$(" #COMMUNITY ").click(function () {
+	$(" #drop5 ").toggleClass('show');
 })
