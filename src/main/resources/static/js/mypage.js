2e2ba2f1aeb21ab745e4b7649e72a814af6860ea
Jack <pjh31208@naver.com>
2021-12-17 18:26:14 +0900

    MyPage

---
+++ src/main/resources/static/js/mypage.js
@@ -0,0 +1,21 @@
+$("#list1").click(function() {
+  $("html, body").animate({ scrollTop: 700}, "slow");
+  return false;
+});
+
+$("#list2").click(function() {
+  $("html, body").animate({ scrollTop: 1200}, "slow");
+  return false;
+});
+
+
+$("#list3").click(function() {
+  $("html, body").animate({ scrollTop: 1900}, "slow");
+  return false;
+});
+$("#list4").click(function() {
+  $("html, body").animate({ scrollTop: 2600}, "slow");
+  return false;
+});
+
+
